b3dd6a8764877d803d5a0ac26d8b985bb12a3634
SeriousM <masta2ooo@gmail.com>
2013-05-03 21:25:20 +0200

    Changed the build up of the tests;

--- test/bf3_test.rb
+++ test/bf3_test.rb
@@ -5,33 +5,31 @@ require 'minitest/autorun'
 include PStats
 
 describe Bf3::Player do
-  before do
-    @player = Bf3::Player.new('SeriousM', 'pc')
-    @non_player = Bf3::Player.new('xx', 'xx')
-  end
+  let(:player) { Bf3::Player.new 'SeriousM', 'pc' }
+  let(:non_player) { Bf3::Player.new 'xx', 'xx' }
 
   describe 'getting a player' do
     it 'should return the player requested' do
-      soldier = @player.load()
-      soldier['country'].wont_be_nil
+      player = @player.load
+      player['country'].wont_be_nil
     end
   end
 
   describe 'getting a player\'s stats' do
     it 'should return the player requested' do
-      soldier = @player.load()
-      soldier['stats'].wont_be_nil
+      stats = player.stats
+      stats.wont_be_nil
     end
+
     it 'should have some global stats' do
-      soldier = @player.stats('clear,rank')
-      soldier['global'].must_be_nil
-      soldier['rank'].wont_be_nil
+      player.load('clear,global')
+      player.current['global'].wont_be_nil
     end
   end
 
   describe 'some vanity stats' do
     it 'should return a floating point kill to death ratio' do
-      kdr = @player.kdr()
+      kdr = player.kdr
       kdr.wont_be_nil
       kdr.must_be_instance_of(Float)
     end
