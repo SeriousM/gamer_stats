b1840207f120688707ec5e706cf1b662c9c22a1d
SeriousM <masta2ooo@gmail.com>
2013-05-03 18:17:01 +0200

    Moved the bf3 file;

--- lib/bf3/bf3.rb
+++ lib/bf3/bf3.rb
@@ -1,6 +1,6 @@
-require "p_stats/version"
 require 'httparty'
 require 'json'
+require 'p_stats'
 
 module PStats
   module Bf3
@@ -19,24 +19,12 @@ module PStats
         @stats ||= stats!(opt)
       end
 
-      def load(opt='clear,global')
-        @player_stats ||= load!(opt)
+      def load!(opt='clear,global')
+        @player_stats = raw_load(opt)
       end
 
-      def load!(opt='clear,global')
-        body = {
-          player: @name,
-          output: 'json',
-          opt: opt
-        }
-        response = HTTParty.post("http://api.bf3stats.com/#{@platform}/player/", :body => body)
-        
-        if response.code == 200 && response['status'] == "data"
-          player_stats = JSON(response.body)
-          @player_stats = player_stats
-        else
-          raise ArgumentError, "Bf3: #{response['error']}"
-        end
+      def load(opt='clear,global')
+        @player_stats ||= load!(opt)
       end
 
       def kdr
@@ -44,6 +32,27 @@ module PStats
         return @stats['global']['kills'].to_f / @stats['global']['deaths']
       end
       
+    private
+
+      def raw_load(opt='clear,global')
+        body = {
+          player: @name,
+          output: 'json',
+          opt: opt
+        }
+
+        begin
+          response = HTTParty.post("http://api.bf3stats.com/#{@platform}/player/", :body => body, timeout: 5)
+        rescue => e
+          raise PStatsError, "Error on loading the player: #{e.message}"
+        end
+        
+        if response.code == 200 && response['status'] == "data"
+          JSON(response.body)
+        else
+          raise PStatsError, "Bf3: #{response['error']}"
+        end
+      end
     end
   end
 end
