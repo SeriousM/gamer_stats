f97dc606bfb9b5694ea4af04a40706234c384e73
SeriousM <masta2ooo@gmail.com>
2013-05-05 08:36:40 +0200

    Specified the required files one-by-one to prevent errors in other projects;

--- lib/gamer_stats.rb
+++ lib/gamer_stats.rb
@@ -1,4 +1,9 @@
-Dir['./**/*.rb'].each{ |f| require f }
+require 'bf3/bf3'
+require 'error/gamer_stats_error'
+require 'gamer_stats/version'
+require 'helper/deep_dup'
+require 'helper/deep_merge'
+require 'helper/hash_path_eval'
 
 module GamerStats
 end
