36cb5ff9511e321092f750e4518b6fe8e23c78b1
SeriousM <masta2ooo@gmail.com>
2013-05-27 00:15:02 +0200

    Start of complete renewal of the bf3 handling;

--- lib/bf3/bf3.rb
+++ lib/bf3/bf3.rb
@@ -5,72 +5,72 @@ require 'gamer_stats'
 module GamerStats
   module Bf3
     class Player
+      class << self
+        def parse(stream)
+          new(JSON(stream))
+        end
 
-      def initialize(name, platform, player = {})
-        @name = name
-        @platform = platform
+        def load(platform, name)
+          new.initial_load(platform, name)
+        end
+
+        private :new
+      end
+
+      def initialize(player = {})
         @player = player.deep_dup
       end
 
-      def current
-        get_current
+      def name
+        @player["name"]
       end
 
-      def stats!
-        raw_load
-        get_current 'stats'
+      def platform
+        @player["plat"]
       end
 
-      def stats
-        raw_load unless loaded? 'stats'
-        get_current 'stats'
-      end
-
-      def load!(opt='clear,global')
-        raw_load(opt)
-        get_current
-      end
-
-      def load(opt='clear,global')
-        raw_load(opt) unless loaded?
-        get_current
-      end
-
-      def kdr
-        load
-        return get_current('stats/global/kills').to_f / get_current('stats/global/deaths')
-      end
-      
     private
 
-      def loaded?(path='')
-        val = @player.path(path)
-        false if val.nil? or val.empty?
+      def load_data_def
+        data_def_path = "#{File.expand_path(File.dirname(__FILE__))}/../res/bf3.datadef"
+        JSON.parse(IO.read(data_def_path))
       end
 
-      def get_current(path='')
-        @player.deep_dup.path(path)
+      def initial_load(platform, name)
+        @player = load_data_def
+        local_player = raw_load('clear,raw,nozero,dogtags,imgInfo')
+        extract_base_data local_player
+        easy_merge local_player
+        key_merge local_player
       end
 
-      def merge(player)
-        @player.deep_merge player
+      def extract_base_data(player)
+        player_copy = player.deep_dup
+        player_copy.delete "stat"
+        player_copy.delete "coop"
+
+        # todo: copy basic info here
       end
 
-      def raw_load(opt='clear,global')
+      def key_merge(player)
+        # todo: do a merge with player-key to @player-data-hash
+      end
+
+      def raw_load(opt='clear')
         body = {
-          player: @name,
+          player: name,
           output: 'json',
           opt: opt
         }
 
         begin
-          response = HTTParty.post("http://api.bf3stats.com/#{@platform}/player/", :body => body, timeout: 5)
+          response = HTTParty.post("http://api.bf3stats.com/#{platform}/player/", :body => body, timeout: 5)
         rescue => e
           raise GamerStatsError, "Error on loading the player: #{e.message}"
         end
         
         if response.code == 200 && response['status'] == "data"
-          @player = merge JSON(response.body)
+          JSON(response.body)
         else
           raise GamerStatsError, "Bf3: #{response['error']}"
         end
