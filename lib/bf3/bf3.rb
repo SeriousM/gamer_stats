cd93c4951b698190dd96edf0985a3d53a41bda13
SeriousM <masta2ooo@gmail.com>
2013-06-25 23:28:52 +0200

    Changed the load of the bf3 datadef from pure file to gzip file; Added more logic into the load process;

--- lib/bf3/bf3.rb
+++ lib/bf3/bf3.rb
@@ -1,24 +1,19 @@
 require 'httparty'
 require 'json'
+require 'yaml'
 require 'gamer_stats'
+require 'helper/zip'
 
 module GamerStats
   module Bf3
     class Player
-      class << self
-        def parse(stream)
-          new(JSON(stream))
-        end
-
-        def load(platform, name)
-          new.initial_load(platform, name)
-        end
-
-        private :new
+      
+      def self.parse(stream)
+        new(JSON(stream))
       end
 
-      def initialize(player = {})
-        @player = player.deep_dup
+      def self.load(platform, name)
+        new.initial_load(platform, name)
       end
 
       def name
@@ -29,31 +24,68 @@ module GamerStats
         @player["plat"]
       end
 
+      def to_yaml
+        @player.to_yaml
+      end
+
+      def to_s
+        @player.to_s
+      end
+
     private
 
+      def initialize(player = {})
+        @player = player.deep_dup
+      end
+
       def load_data_def
-        data_def_path = "#{File.expand_path(File.dirname(__FILE__))}/../res/bf3.datadef"
-        JSON.parse(IO.read(data_def_path))
+        data_def_path = "#{File.expand_path(File.dirname(__FILE__))}/../res/bf3.datadef.gz"
+        file_content = Util::Zip.ungzip(data_def_path)
+        JSON.parse(file_content)
       end
 
       def initial_load(platform, name)
         @player = load_data_def
-        local_player = raw_load('clear,raw,nozero,dogtags,imgInfo')
-        extract_base_data local_player
+        local_player = raw_load('clear,raw,nozero,dogtags,imgInfo,nextranks')
         easy_merge local_player
+        dogtag_merge local_player
+        nextrank_merge local_player
         key_merge local_player
       end
 
-      def extract_base_data(player)
-        player_copy = player.deep_dup
-        player_copy.delete "stat"
-        player_copy.delete "coop"
+      def easy_merge(from)
+        ["plat","name","tag","language","country",
+         "country_name","country_img",
+         "date_insert","date_update"].each do |key|
+          
+          @player[key] = from[key]
+        end
 
-        # todo: copy basic info here
+        to[status]
       end
 
-      def key_merge(player)
-        # todo: do a merge with player-key to @player-data-hash
+      def dogtag_merge(from)
+        ["basic","advanced"].each do |cat|
+          ["name","desc","license","image_l","image_s"].each do |key|
+            @player["dogtag"][cat][key] = from["dogtag"][cat][key]
+          end
+        end
+      end
+
+      def nextrank_merge(from)
+        @player["stats"]["nextranks"] = from["stats"]["nextranks"]
+      end
+
+      def key_merge(from)
+        player_string = @player.to_yaml
+
+        ["stat","coop"].each do |cat|
+          from[cat].each_pair do |key, value|
+            player_string.sub! "#{cat}.#{key}", value
+          end
+        end
+
+        @player = YAML.load(player_string)
       end
 
       def raw_load(opt='clear')
